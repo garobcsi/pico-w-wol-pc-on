32011a850e1ba7aef3eae4b3c6e88e7776e41351
garobcsi <gasparrobert0908@gmail.com>
2024-07-29 19:02:31 +0200

    added cmake to build uf2 and extended hello world

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ int main() {
         printf("Wi-Fi init failed");
         return -1;
     }
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
 }
